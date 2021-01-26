bfea44e91b65a3c8e3d63ee337931d129c42c7eb
Alejandro Ocampo Cisneros <ocampoalejandro.oc@gmail.com>
2021-01-25 22:09:38 -0600

    Programa Beta1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,25 +31,34 @@
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnImportaer2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(26, 139);
+            this.button1.Location = new System.Drawing.Point(26, 288);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(106, 69);
             this.button1.TabIndex = 0;
             this.button1.Text = "Seleccioinar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(262, 41);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(755, 509);
+            this.pictureBox1.Size = new System.Drawing.Size(541, 428);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
             // button2
             // 
@@ -61,11 +70,42 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(26, 385);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 69);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Match";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnImportaer2
+            // 
+            this.btnImportaer2.Location = new System.Drawing.Point(26, 145);
+            this.btnImportaer2.Name = "btnImportaer2";
+            this.btnImportaer2.Size = new System.Drawing.Size(106, 69);
+            this.btnImportaer2.TabIndex = 4;
+            this.btnImportaer2.Text = "Importar2";
+            this.btnImportaer2.UseVisualStyleBackColor = true;
+            this.btnImportaer2.Click += new System.EventHandler(this.btnImportaer2_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(829, 41);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(541, 428);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1123, 583);
+            this.ClientSize = new System.Drawing.Size(1401, 583);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btnImportaer2);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
@@ -73,6 +113,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -82,6 +123,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnImportaer2;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,19 +7,173 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
 
 namespace Encuentra_este_objeto
 {
     public partial class Form1 : Form
     {
+        Image<Bgr, byte> img;
+
+        Dictionary<string, Image<Bgr,byte>> imgList;
+        Rectangle rect;
+        Point StartROI, EndROI;
+        bool Selecting;
+        bool MouseDown;
+
         public Form1()
         {
             InitializeComponent();
+            Selecting = false;
+            rect = Rectangle.Empty;
+            imgList = new Dictionary<string, Image<Bgr, byte>>();
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            OpenFileDialog 
+            imgList.Clear();
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog()==DialogResult.OK)
+            {
+                img = new Image<Bgr, byte>(ofd.FileName);
+                pictureBox1.Image = img.ToBitmap();
+                AddImage(img, "Input");
+            }
+        }
+
+
+        private void AddImage(Image<Bgr,byte> img, string keyname)
+        {
+             if(!imgList.ContainsKey(keyname))
+            {
+                imgList.Add(keyname, img);
+            }
+            else
+            {
+                imgList[keyname] = img;
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(Selecting)
+            {
+                MouseDown = true;
+                StartROI = e.Location;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(Selecting)
+            {
+                int width = Math.Max(StartROI.X, e.X) - Math.Min(StartROI.X, e.X);
+                int Height = Math.Max(StartROI.Y, e.Y) - Math.Min(StartROI.Y, e.Y);
+                rect = new Rectangle(Math.Min(StartROI.X,e.X),Math.Min(StartROI.Y,e.Y),width,Height);
+                Refresh();
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if(MouseDown)
+            {
+                using (Pen pen = new Pen(Color.Red))
+                {
+                    e.Graphics.DrawRectangle(pen,rect);
+                }
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(Selecting)
+            {
+                Selecting = false;
+                MouseDown = false;
+            }
+
+            ObtenerImagen();
+        }
+
+
+        private void ObtenerImagen()
+        {
+            try
+            {
+                if(pictureBox1.Image == null)
+                {
+                    return;
+                }
+                if(rect==Rectangle.Empty)
+                {
+                    return;
+                }
+                var img = new Image<Bgr, byte>(new Bitmap(pictureBox1.Image));
+
+                img.ROI = rect;
+
+                var imgROI = img.Copy();
+                img.ROI = Rectangle.Empty;
+
+                pictureBox1.Image = imgROI.ToBitmap();
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(pictureBox1.Image==null || rect ==null)
+                {
+                    return;
+                }
+
+                var imgScene = new Image<Bgr, byte>(new Bitmap(pictureBox2.Image));
+                var template = new Image<Bgr,byte>( new Bitmap(pictureBox1.Image));
+
+                Mat imgOut = new Mat();
+
+                CvInvoke.MatchTemplate(imgScene,template,imgOut,Emgu.CV.CvEnum.TemplateMatchingType.Sqdiff);
+                double minVal = 0.0;
+                double maxVal = 0.0;
+                Point minLoc = new Point();
+                Point maxLoc = new Point();
+                CvInvoke.MinMaxLoc(imgOut, ref minVal, ref maxVal, ref minLoc, ref maxLoc);
+                Rectangle r = new Rectangle(minLoc, template.Size);
+                CvInvoke.Rectangle(imgScene, r, new MCvScalar(255, 0, 0), 3);
+                pictureBox1.Image = imgScene.ToBitmap();
+
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnImportaer2_Click(object sender, EventArgs e)
+        {
+            //imgList.Clear();
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                img = new Image<Bgr, byte>(ofd.FileName);
+                pictureBox2.Image = img.ToBitmap();
+                //AddImage(img, "Input");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Selecting = true;
+            
         }
     }
 }
